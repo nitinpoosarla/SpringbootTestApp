45008ca9440638a6c94de5f4a09a1997b1fdd9a9
nitin.poosarla <nitin.poosarla@ADG-L35.Ajman.ae>
2020-07-23 23:55:29 +0400

    WH-8520 - First Time Commit

---
+++ src/main/java/in/nit/MessageRunner.java
@@ -0,0 +1,15 @@
+package in.nit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements  CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to springboot app");
+		
+	}
+
+}
