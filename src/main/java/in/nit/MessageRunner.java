5564176b9b2cc1faebcdd67e613521323e2c9cac
nitin.poosarla <nitin.poosarla@ADG-L35.Ajman.ae>
2020-07-24 00:08:50 +0400

    WH 9988 - third commit

--- src/main/java/in/nit/MessageRunner.java
+++ src/main/java/in/nit/MessageRunner.java
@@ -9,7 +9,7 @@ public class MessageRunner implements  CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("welcome to springboot app");
-		
+		int a=89;
 		System.out.println("modified to springboot app");
 		
 	}
